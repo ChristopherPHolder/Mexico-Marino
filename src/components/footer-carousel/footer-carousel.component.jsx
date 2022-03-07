c19e10ece57f64bf823ce066e34acd05e2aaae9c
Christopher Holder <toholderandrews@gmail.com>
2022-03-07 23:35:22 +0100

    feat: add base for footer carousel banner

---
+++ src/components/footer-carousel/footer-carousel.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './footer-carousel.styles.scss';
+
+const FooterCarousel = () => {
+    return (
+        <section className='footer-carousel'>
+            Testing
+        </section>
+    )
+};
+
+export default FooterCarousel;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import HeroBanner from '../components/hero-banner/hero-banner.component';
 import FeaturedListings from '../components/featured-listings/featured-listings.component';
 
 import Layout from '../components/layout/layout.component';
+import FooterCarousel from '../components/footer-carousel/footer-carousel.component';
 
 const Home = () => {
   return (
     <Layout>
       <HeroBanner />
       <FeaturedListings />
+      <FooterCarousel />
     </Layout>
   )
 }

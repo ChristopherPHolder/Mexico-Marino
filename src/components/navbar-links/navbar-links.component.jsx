687c1cae07e7bf331175fa5f02a27b71166c7006
Christopher Holder <toholderandrews@gmail.com>
2022-03-10 16:28:28 +0100

    feat: redirect to external link for climate and contact form

--- src/components/navbar-links/navbar-links.component.jsx
+++ src/components/navbar-links/navbar-links.component.jsx
@@ -4,27 +4,36 @@ import { Link } from 'gatsby';
 import SearchIcon from '../../assets/svg/icons8-search.svg';
 
 import './navbar-links.styles.scss';
+
 const NavbarLinks = () => {
   const sections = [
-    {sectionName: 'EMBARCATIONES', link: '/'},
-    {sectionName: 'DISTRIBUDORES', link: '/'},
-    {sectionName: 'PRODUCTOS', link: '/'},
-    {sectionName: 'CLIMA', link: '/'},
-    {sectionName: 'SERVICIOS', link: '/'},
-    {sectionName: 'CONTACTO', link: '/'}
+    {sectionName: 'EMBARCACIONES', location: 'Internal', link: '/'},
+    {sectionName: 'DISTRIBUDORES', location: 'Internal', link: '/'},
+    {sectionName: 'PRODUCTOS', location: 'Internal', link: '/'},
+    {sectionName: 'CLIMA', location: 'External', link: 'https://mexicomarino.com/clima/informacion'},
+    {sectionName: 'SERVICIOS', location: 'Internal', link: '/'},
+    {sectionName: 'CONTACTO', location: 'External', link: 'https://mexicomarino.com/contacto'}
   ]
 
   return (
     <div className='navbar-links'>
 
       { sections.map((section) => (
-        <Link>
-          <span className='navbar-link'>
-            { section.sectionName === 'EMBARCATIONES' ?
-            <SearchIcon className='search-icon'/> : '' }
-            {section.sectionName}
-          </span>
-        </Link>
+        <spam>
+          { 
+            section.location === 'External' ?
+            
+              <a href={section.link} className='navbar-link'>
+                {section.sectionName}
+              </a> 
+            :
+              <Link to={section.link} className='navbar-link'>
+                  { section.sectionName === 'EMBARCACIONES' ?
+                    <SearchIcon className='search-icon'/> : '' }
+                  {section.sectionName}
+              </Link>
+          }
+        </spam>
       ))}
 
     </div>
